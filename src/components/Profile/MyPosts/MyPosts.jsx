f242b7b64d853ca8c879e9d05fa565038bfab127
ekambalov <emakcomp@gmail.com>
2022-03-29 08:40:21 +0300

    feat: add onClick on post button l31

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,14 +3,23 @@ import classes from './MyPosts.module.css'
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
+
     let posts = props.data
         .map(data => <Post message={data.message} likes={data.likesCounter} />)
+
+    let newElement = React.createRef()
+
+    const addPost = ()=>{
+        let text = newElement.current.value;
+        alert(text);
+    }
+
     return(
         <div> 
             <div className={classes.input_wrapper}>
                 <h2 className={classes.title}>My Posts</h2>
-                <textarea   placeholder='your news' className={classes.input}/>
-                <button className={classes.button}>send</button>
+                <textarea ref={newElement}  placeholder='your news' className={classes.input}/>
+                <button onClick={addPost} className={classes.button}>send</button>
             </div>
             {posts}
         </div>
