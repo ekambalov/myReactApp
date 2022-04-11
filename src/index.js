61b5f3965a460450b6b6aba4769188d81ff0c002
ekambalov <emakcomp@gmail.com>
2022-04-11 18:42:22 +0200

    feat: create store

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import state, { subscribe } from './redux/state'
+import store from './redux/state'
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import {addPost, updateNewPostChange} from './redux/state'
 
-const renderEntireTrie = (state) => {
+let renderEntireTrie = (state) => {
 
   ReactDOM.render(
     <React.StrictMode>
       <App data={state} 
-      addPost={addPost} 
-      updateNewPostChange={updateNewPostChange} />
+      addPost={store.addPost.bind(store)} 
+      updateNewPostChange={store.updateNewPostChange.bind(store)} />
     </React.StrictMode>,
     document.getElementById('root')
   );
 
 }
 
-renderEntireTrie(state);
-subscribe(renderEntireTrie)
+renderEntireTrie(store.getState());
+store.subscribe(renderEntireTrie)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,56 +1,64 @@
-let state = { profilePage:
-    {
-        postData: [
-            {id:0, message:"fuck you, russian warship!", likesCounter:1337},
-            {id:1, message:"Hellow, world", likesCounter:10},
-            {id:2, message:"It's my first post", likesCounter:0},
-            {id:3, message:"fuck fuck", likesCounter:0},
-            {id:4, message:"fuck", likesCounter:0}
-        ],
-        newPostText: ''
+let store = {
+    _state : { profilePage:
+        {
+            postData: [
+                {id:0, message:"fuck you, russian warship!", likesCounter:1337},
+                {id:1, message:"Hellow, world", likesCounter:10},
+                {id:2, message:"It's my first post", likesCounter:0},
+                {id:3, message:"fuck fuck", likesCounter:0},
+                {id:4, message:"fuck", likesCounter:0}
+            ],
+            newPostText: ''
+        },
+        dialogsPage : {
+            dialogsData : [
+              {id: 1, name: "Zmicer"},
+              {id: 2, name: "Jahor"},
+              {id: 3, name: "Julia"},
+              {id: 4, name: "Mikita"},
+              {id: 5, name: "Hanna"}
+            ],
+            messagesData : [
+              {id:1, message: 'Hi!'},
+              {id:2, message: 'How are you?'},
+              {id:3, message: 'wtf, man?!'},
+              {id:4, message: 'it\'s not of your busnisses'},
+              {id:5, message: 'yo'}
+            ]
+        }
+      },
+      getState(){
+          return this._state;
+      },
+     _callSubscriber() {
+        console.log('state change')
     },
-    dialogsPage : {
-        dialogsData : [
-          {id: 1, name: "Zmicer"},
-          {id: 2, name: "Jahor"},
-          {id: 3, name: "Julia"},
-          {id: 4, name: "Mikita"},
-          {id: 5, name: "Hanna"}
-        ],
-        messagesData : [
-          {id:1, message: 'Hi!'},
-          {id:2, message: 'How are you?'},
-          {id:3, message: 'wtf, man?!'},
-          {id:4, message: 'it\'s not of your busnisses'},
-          {id:5, message: 'yo'}
-        ]
+    addPost() {
+        let newPost = {
+            id: this._state.profilePage.postData.length + 1,
+            message: this._state.profilePage.newPostText,
+            likesCounter: 0
+        }
+        this._state.profilePage.postData.push(newPost);
+        this._callSubscriber(this._state)
+        this.updateNewPostChange('');
+    },
+    updateNewPostChange(newText) {
+
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state)
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
-  }
-
-let renderEntireTrie = (state)=>{
-    console.log('state change')
-}
-
-export const addPost = () => {
-    let newPost = {
-        id: state.profilePage.postData.length + 1,
-        message: state.profilePage.newPostText,
-        likesCounter: 0
-    }
-    state.profilePage.postData.push(newPost);
-    renderEntireTrie(state)
-    updateNewPostChange('');
-}
-
-export const updateNewPostChange = (newText) => {
-
-    state.profilePage.newPostText = newText;
-    renderEntireTrie(state)
-}
-
-export const subscribe = (observer) =>{
-    renderEntireTrie = observer;
 }
 
 
-export default state;
+
+
+
+
+
+
+export default store;
+window.store = store;
