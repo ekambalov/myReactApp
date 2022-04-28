5d2082b548549a2661bfdb60a0e22887528b4777
ekambalov <emakcomp@gmail.com>
2022-04-28 22:10:00 +0200

    feat: less 54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -7,12 +7,16 @@ class Users extends React.Component {
 
     constructor(props) {
         super(props);
+
+
+
+    }
+
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
                 this.props.setUsers(response.data.items)
     
             }) 
-
-
     }
 
     getUsers = () => {
